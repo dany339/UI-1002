6d6d60ecf9a603db316754bcaa50315139e840a9
dany339 <hyd55080001@gmail.com>
2024-10-29 11:23:15 +0900

    이소룡씨 움직이기

--- 12-No-Plan-B/js/script.js
+++ 12-No-Plan-B/js/script.js
@@ -12,12 +12,16 @@ $(function () {
     tl.from(".copyright", { x: -50, autoAlpha: 0 }, "-=0.4");
 
     // bruce lee
-    tl.from(".bruce-lee-bg", {
-        autoAlpha: 0,
-        scale: 0.9,
-        duration: 3,
-        ease: "none",
-    });
+    tl.from(
+        ".bruce-lee-bg",
+        {
+            autoAlpha: 0,
+            scale: 0.9,
+            duration: 3,
+            ease: "none",
+        },
+        1 // 전체 타임라인의 1초 지점에서 시작
+    );
 
     tl.from(".bruce-lee", {
         autoAlpha: 0,
@@ -26,7 +30,14 @@ $(function () {
     });
 
     // title
-    tl.from(".title h2 strong", { x: -50, autoAlpha: 0, duration: 1 });
+    tl.from(".title h2 strong", {
+        x: -50,
+        autoAlpha: 0,
+        duration: 1,
+        onComplete: () => {
+            Splitting();
+        },
+    });
 
     tl.from(".title h2 span", { x: -50, autoAlpha: 0, duration: 1 }, "-=0.8");
 
@@ -34,13 +45,64 @@ $(function () {
         xPercent: 200 /* 200% */,
         duration: 0.7,
         ease: "elastic.out(1,0.3)",
+        onComplete: () => {
+            $(".small-bruce-lee").addClass("action");
+            moving();
+        },
     });
 
     // 여기 skip 할 때 도달할 지점
     tl.addLabel("end");
 
+    tl.from(".bth-skip", { x: -100, autoAlpha: 0 }, 0);
+
     // skip 클릭했을 때
     $(".btn-skip").on("click", () => {
         tl.seek("end");
+        Splitting();
+        $(".small-bruce-lee").addClass("action");
+        moving();
     });
+
+    // 괴조음
+    const scream = $(".scream").get(0);
+    $(".small-bruce-lee").on("click", () => scream.play());
+
+    // 이소룡 움직이기
+    const $window = $(window);
+    let x = 0;
+    let y = 0;
+    let mx = 0;
+    let my = 0;
+    const speed = 0.09;
+
+    // 마우스가 움직이면 좌표값을 구한다.
+    $window.on("mousemove", (e) => {
+        // x = e.pageX - $window.innerWidth() / 2; /* 화면의 중심을 기준으로 만들기 위한 코드 */
+        // y = e.pageY - $window.innerHeight() / 2;
+
+        /* 화면 중심을 0,0으로 했으니까, 왼쪽으로 쭉 가도(-960).. 고로 더 큰 -100을 선택. 걍 마지노선 정해준거라 생각해~ */
+        x = Math.max(-100, Math.min(500, e.pageX - $window.innerWidth() / 2));
+        y = Math.max(-10, Math.min(100, e.pageY - $window.innerHeight() / 2));
+    });
+
+    // 대상을 반복적으로 움직이게 만드는 함수
+    function moving() {
+        mx += (x - mx) * speed;
+        my += (y - my) * speed;
+
+        $(".bruce-lee").css({
+            transform: `translateX(${mx}px)` /* 백틱(`) 안에서는 불러올 때 ${}를 사용한다 */,
+        });
+
+        $(".bruce-lee-bg").css({
+            transform: `translate(${mx * 0.4}px, ${my}px)`,
+        });
+
+        $(".title").css({
+            transform: `translate(${-mx * 0.4}px, -50%)` /* css에서 transformY(-50%)를 줬기 때문 */,
+        });
+
+        requestAnimationFrame(moving);
+    }
 });
